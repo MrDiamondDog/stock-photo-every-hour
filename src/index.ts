d5e5ffd1e5c0d8206806c6a54cc54e61f1ee017b
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2025-03-24 21:31:32 -0600

    switch stock image provider and make use of cron

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,29 @@ function rand(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function randOf(arr: any[]) {
+    return arr[rand(0, arr.length - 1)];
+}
+
 async function getRandomImage() {
-    const res = await fetch(`https://customsearch.googleapis.com/customsearch/v1?cx=${process.env.SEARCH_ID!}&searchType=image&q=stock+image&siteSearch=investopedia.com,upload.wikimedia.org&siteSearchFilter=e&key=${process.env.SEARCH_KEY!}&start=${rand(0, 191)}`)
+    // const res = await fetch(`https://customsearch.googleapis.com/customsearch/v1?cx=${process.env.SEARCH_ID!}&searchType=image&q=stock+image&siteSearch=investopedia.com,upload.wikimedia.org&siteSearchFilter=e&key=${process.env.SEARCH_KEY!}&start=${rand(0, 191)}`)
+    // const endpoints = ["royalty-free", "public-domain", "creative-common", "high-resolution", "creative", "non-copyrighted", "blogging"]
+
+    // const res = await fetch(`
+    //     https://unsplash.com/napi/landing_pages/images/stock/${randOf(endpoints)}?page=${rand(1, 1000)}&per_page=30
+    // `);
+
+    // very basic and broad terms to search for that are not specific in any way
+    const searchTerms = ["people", "person", "building", "city", "nature", "landscape", "animals", "travel", "business", "technology"];
+
+    const res = await fetch(`
+        https://www.alamy.com/search-api/search/?qt=${randOf(searchTerms)}&sortBy=relevant&ispartial=true&langcode=en&isbot=false&type=picture&geo=US&nasty=0&editorial=1&pn=${rand(1, 500)}&ps=100
+        `)
     const data = await res.json();
 
-    const images = data.items.map((item: any) => item.link);
+    console.log(data);
+
+    const images = data.items.map((item: any) => item.renditions["450v"].href);
     return images[rand(0, images.length - 1)];
 }
 
@@ -36,7 +54,7 @@ async function postImage(imageUrl: string) {
     const image = await client.v1.uploadMedia(buffer, {
         mimeType: "image/jpeg",
     });
-    
+
     // Post tweet
     await client.v2.tweet("", {
         media: {
@@ -45,6 +63,10 @@ async function postImage(imageUrl: string) {
     });
 }
 
-// Schedule to run every hour
-setInterval(async () => postImage(await getRandomImage()), 1000 * 60 * 60);
-(async () => postImage(await getRandomImage()))();
+(async () => {
+    const imageUrl = await getRandomImage();
+    console.log(`Got image: ${imageUrl}`);
+
+    await postImage(imageUrl);
+    console.log("Posted image successfully");
+})();
